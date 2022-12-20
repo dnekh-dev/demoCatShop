abe47d1cdfe8b1f965003463e99c5ed866219af9
d_nekh <dnekh.dev@gmail.com>
2022-12-20 19:24:28 +0900

    Zero commit

---
+++ src/test/java/com/dnekh/democatshop/DemoCatShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnekh.democatshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCatShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
