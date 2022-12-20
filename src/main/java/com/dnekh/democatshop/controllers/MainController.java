2da573124c936c69895582a59c7ab5021ed909ea
d_nekh <dnekh.dev@gmail.com>
2022-12-20 20:18:54 +0900

    Here was created MainController class and index HTML file

---
+++ src/main/java/com/dnekh/democatshop/controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.dnekh.democatshop.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping
+    public String getIndex() {
+        return "index";
+    }
+}
