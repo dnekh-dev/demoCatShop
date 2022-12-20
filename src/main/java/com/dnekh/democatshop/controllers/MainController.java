ed323039f9a8a9e69cadb6134a5f8b837308e321
d_nekh <dnekh.dev@gmail.com>
2022-12-20 21:13:44 +0900

    Here were added aboutUs html file and getAboutUs method in MainController class

--- src/main/java/com/dnekh/democatshop/controllers/MainController.java
+++ src/main/java/com/dnekh/democatshop/controllers/MainController.java
@@ -1,13 +1,21 @@
 package com.dnekh.democatshop.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
-    @GetMapping
-    public String getIndex() {
+    @GetMapping("/")
+    public String getIndex(Model model) {
+        model.addAttribute("hello", "Hello Kitty Super Mors!");
         return "index";
+    }
+
+    @GetMapping("/about-us")
+    public String getAboutUs(Model model) {
+        model.addAttribute("about", "Here is you can find all needing information about us!");
+        return "aboutUs";
     }
 }
